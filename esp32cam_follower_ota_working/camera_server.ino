90bee12382704b84e284163986149c31ca73b998
Dirk-Willem van Gulik <dirkx@webweaving.org>
2022-10-12 13:42:50 +0200

    Rough re-arranging to make introducing PID a bit easier

--- esp32cam_follower_ota_working/camera_server.ino
+++ esp32cam_follower_ota_working/camera_server.ino
@@ -4,78 +4,85 @@
 static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
 static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
 static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
- 
+
 httpd_handle_t stream_httpd = NULL;
- 
-static esp_err_t stream_handler(httpd_req_t *req){
+
+static void(*_frameHandlerCallBack)(camera_fb_t * current_frame) = NULL;
+
+static esp_err_t stream_handler(httpd_req_t *req)
+{
   camera_fb_t * fb = NULL;
   esp_err_t res = ESP_OK;
   size_t _jpg_buf_len = 0;
   uint8_t * _jpg_buf = NULL;
   char * part_buf[64];
- 
+
   res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if(res != ESP_OK){
+  if (res != ESP_OK) {
     return res;
   }
- 
-  while(true){
+
+  while (true) {
     fb = esp_camera_fb_get();
- 
+
     if (!fb) {
       Serial.println("Camera capture failed");
       res = ESP_FAIL;
     } else {
-          //camera_fb_t * rf =  fb; //set to fb to test
-          camera_fb_t * rf =  diff(fb); 
+      //camera_fb_t * rf =  fb; //set to fb to test
+      camera_fb_t * rf = fb;
+      if (_frameHandlerCallBack)
+        (*_frameHandlerCallBack)(fb);
 
-          bool jpeg_converted = frame2jpg(rf, 80, &_jpg_buf, &_jpg_buf_len);
+      bool jpeg_converted = frame2jpg(rf, 80, &_jpg_buf, &_jpg_buf_len);
 
-          ///esp_camera_fb_return(fb);
-          fb = NULL;
-          if(!jpeg_converted){
-            Serial.println("JPEG compression failed");
-            res = ESP_FAIL;
-          }
+      ///esp_camera_fb_return(fb);
+      fb = NULL;
+      if (!jpeg_converted) {
+        Serial.println("JPEG compression failed");
+        res = ESP_FAIL;
+      }
     }
-    if(res == ESP_OK){
+    if (res == ESP_OK) {
       size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
       res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
     }
-    if(res == ESP_OK){
+    if (res == ESP_OK) {
       res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
     }
-    if(res == ESP_OK){
+    if (res == ESP_OK) {
       res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
     }
-    if(fb){
+    if (fb) {
       esp_camera_fb_return(fb);
       fb = NULL;
       _jpg_buf = NULL;
-    } else if(_jpg_buf){
+    } else if (_jpg_buf) {
       free(_jpg_buf);
       _jpg_buf = NULL;
     }
-    if(res != ESP_OK){
+    if (res != ESP_OK) {
       break;
     }
     //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
   }
   return res;
 }
- 
-void startCameraServer(){
+
+void startCameraServer(void(*processFrameCallback)(camera_fb_t * current_frame))
+{
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
   config.server_port = 80;
- 
+
+  _frameHandlerCallBack = processFrameCallback;
+
   httpd_uri_t index_uri = {
     .uri       = "/",
     .method    = HTTP_GET,
     .handler   = stream_handler,
     .user_ctx  = NULL
   };
-   
-  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+
   if (httpd_start(&stream_httpd, &config) == ESP_OK) {
     httpd_register_uri_handler(stream_httpd, &index_uri);
   }
--- esp32cam_follower_ota_working/esp32cam_follower_ota_working.ino
+++ esp32cam_follower_ota_working/esp32cam_follower_ota_working.ino
@@ -6,17 +6,30 @@
 #include <ArduinoOTA.h>
 #include <ESPmDNS.h>
 
-Servo servo_pan;  
-Servo servo_tilt; 
+Servo servo_pan;
+Servo servo_tilt;
 
 //servo position
-int pos_pan = 90;    
-int pos_tilt = 90;    
+int pos_pan = 90;
+int pos_tilt = 90;
 
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+// position in servo speak
+const float NEUTRAL_POS_X = 0.5;
+const float NEUTRAL_POS_Y = 0.7;
+
+// position of where the camera looks; 0 is left/bottom; 1 is top/right
+const float CENTER_POS_X = 0.5;
+const float CENTER_POS_Y = 0.5;
+
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
 // you have to use a timer higher than 2, see https://www.esp32.com/viewtopic.php?t=11379
-int servoPin_pan = 14; 
-int servoPin_tilt = 2; 
+int servoPin_pan = 14;
+int servoPin_tilt = 2;
+
+// servos - how fast they travel; i.e. how
+// long to wait (at least) between each update
+// of one degree.
+const  int wait = 15; // mSeconds *
 
 // Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
 const char *ssid = "XXXXXXX";       // Put your SSID here
@@ -47,98 +60,145 @@ const char *password = "XXXXXXX"; // Put your PASSWORD here
 #define LAMP_PIN           4 // LED FloodLamp.
 
 static camera_config_t camera_config = {
-    .pin_pwdn = PWDN_GPIO_NUM,
-    .pin_reset = RESET_GPIO_NUM,
-    .pin_xclk = XCLK_GPIO_NUM,
-    .pin_sscb_sda = SIOD_GPIO_NUM,
-    .pin_sscb_scl = SIOC_GPIO_NUM,
-    .pin_d7 = Y9_GPIO_NUM,
-    .pin_d6 = Y8_GPIO_NUM,
-    .pin_d5 = Y7_GPIO_NUM,
-    .pin_d4 = Y6_GPIO_NUM,
-    .pin_d3 = Y5_GPIO_NUM,
-    .pin_d2 = Y4_GPIO_NUM,
-    .pin_d1 = Y3_GPIO_NUM,
+  .pin_pwdn = PWDN_GPIO_NUM,
+  .pin_reset = RESET_GPIO_NUM,
+  .pin_xclk = XCLK_GPIO_NUM,
+  .pin_sscb_sda = SIOD_GPIO_NUM,
+  .pin_sscb_scl = SIOC_GPIO_NUM,
+  .pin_d7 = Y9_GPIO_NUM,
+  .pin_d6 = Y8_GPIO_NUM,
+  .pin_d5 = Y7_GPIO_NUM,
+  .pin_d4 = Y6_GPIO_NUM,
+  .pin_d3 = Y5_GPIO_NUM,
+  .pin_d2 = Y4_GPIO_NUM,
+  .pin_d1 = Y3_GPIO_NUM,
 
-    .pin_d0 = Y2_GPIO_NUM,
-    .pin_vsync = VSYNC_GPIO_NUM,
-    .pin_href = HREF_GPIO_NUM,
-    .pin_pclk = PCLK_GPIO_NUM,
-    .xclk_freq_hz = 20000000,
+  .pin_d0 = Y2_GPIO_NUM,
+  .pin_vsync = VSYNC_GPIO_NUM,
+  .pin_href = HREF_GPIO_NUM,
+  .pin_pclk = PCLK_GPIO_NUM,
+  .xclk_freq_hz = 20000000,
 
-    .ledc_timer = LEDC_TIMER_0,
+  .ledc_timer = LEDC_TIMER_0,
 
-    .ledc_channel = LEDC_CHANNEL_0,
+  .ledc_channel = LEDC_CHANNEL_0,
 
-    //https://forum.arduino.cc/t/esp32_cam-acces-and-process-image/677628/6
-    //"you get 19200 bytes with is correct for 160x120 grayscale pixels on 8 bits"
-    .pixel_format = PIXFORMAT_GRAYSCALE,//YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QQVGA,//QQVGA-QXGA Do not use sizes above QVGA when not JPEG
-    .jpeg_quality = 10, //0-63 lower number means higher quality
-    .fb_count = 1 //if more than one, i2s runs in continuous mode. Use only with JPEG
-    //.grab_mode = CAMERA_GRAB_WHEN_EMPTY//CAMERA_GRAB_LATEST. Sets when buffers should be filled [doesn't work]
+  //https://forum.arduino.cc/t/esp32_cam-acces-and-process-image/677628/6
+  //"you get 19200 bytes with is correct for 160x120 grayscale pixels on 8 bits"
+  .pixel_format = PIXFORMAT_GRAYSCALE,//YUV422,GRAYSCALE,RGB565,JPEG
+  .frame_size = FRAMESIZE_QQVGA,//QQVGA-QXGA Do not use sizes above QVGA when not JPEG
+  .jpeg_quality = 10, //0-63 lower number means higher quality
+  .fb_count = 1 //if more than one, i2s runs in continuous mode. Use only with JPEG
+  //.grab_mode = CAMERA_GRAB_WHEN_EMPTY//CAMERA_GRAB_LATEST. Sets when buffers should be filled [doesn't work]
 };
 
-esp_err_t camera_init(){
-    //power up the camera if PWDN pin is defined
-    if(PWDN_GPIO_NUM != -1){
-        pinMode(PWDN_GPIO_NUM, OUTPUT);
-        digitalWrite(PWDN_GPIO_NUM, LOW);
-    }
+esp_err_t camera_init() {
+  //power up the camera if PWDN pin is defined
+  if (PWDN_GPIO_NUM != -1) {
+    pinMode(PWDN_GPIO_NUM, OUTPUT);
+    digitalWrite(PWDN_GPIO_NUM, LOW);
+  }
 
-    //initialize the camera
-    esp_err_t err = esp_camera_init(&camera_config);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Camera Init Failed");
-        return err;
-    }
+  //initialize the camera
+  esp_err_t err = esp_camera_init(&camera_config);
+  if (err != ESP_OK) {
+    ESP_LOGE(TAG, "Camera Init Failed");
+    return err;
+  }
 
-    Serial.println("Cameria initialised");
-    sensor_t * s = esp_camera_sensor_get();
-    s->set_vflip(s, 1);
-    return ESP_OK;
+  Serial.println("Cameria initialised");
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_vflip(s, 1);
+  return ESP_OK;
 }
 
+// Postition of the server - remembered.
+//
+float m_attentionX = NEUTRAL_POS_X;
+float m_attentionY = NEUTRAL_POS_Y;
+
+void processFrame(camera_fb_t * current_frame) {
+
+  // diff() will also update previous_frame with some accomodation of
+  // slowly changing light conditions.
+  //
+  camera_fb_t * difference = diff(current_frame);
+  if (difference == NULL)
+    return; // no difference yet - is the first image captured.
+  float cogX, cogY; // center of gravity
+
+  // Calculate the center of gravity.
+  //
+  int perc = calculate_cog(difference, &cogX, &cogY);
+
+  if (perc < 2) {
+    // low confidence in what we see - let the servos move slowy to the center.
+    //0.5 and 0.7 are neutral positions of the servos, 0-1
+    //20 is what fraction of the distance to move
+    m_attentionX = m_attentionX + (((NEUTRAL_POS_X - m_attentionX)) / 20);
+    m_attentionY = m_attentionY + (((NEUTRAL_POS_Y - m_attentionY)) / 20);
+
+    pan((int)pos_pan, (int)(m_attentionX * 180), wait);
+    tilt((int)pos_tilt, (int)(m_attentionY * 180), wait);
+    return;
+  }
+  Serial.printf("%d percent changed pixels: ", perc);
+
+  Serial.printf("COG is %.2f, %.2f\r\n", cogX, cogY);
+  Serial.printf("Servo attention is %.2f, %.2f\r\n", m_attentionX, m_attentionY);
+
+  // simple mode - move to the right place - we assume that
+  // the camera is fixed.
+  Serial.print("Moving to x ");
+  Serial.print((int)(m_attentionX * 180));
+  Serial.print(" and y ");
+  Serial.println((int)(m_attentionY * 180));
+
+  pan((int)pos_pan, (int)(m_attentionX * 180), wait);
+  tilt((int)pos_tilt, (int)(m_attentionY * 180), wait);
+}
 
 void setup() {
 
-    Serial.begin(115200);
-    Serial.println("starting");
-    esp_log_level_set("*", ESP_LOG_VERBOSE);
-    pinMode(servoPin_pan, OUTPUT);
-    pinMode(servoPin_tilt, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("starting");
+  esp_log_level_set("*", ESP_LOG_VERBOSE);
+  pinMode(servoPin_pan, OUTPUT);
+  pinMode(servoPin_tilt, OUTPUT);
 
-    //https://stackoverflow.com/questions/64402915/esp32-cam-with-servo-control-wont-work-arduino-ide
-    //and
-    //https://www.esp32.com/viewtopic.php?t=11379
-    servo_pan.attach(servoPin_pan, 4); 
-    servo_tilt.attach(servoPin_tilt, 5); 
+  //https://stackoverflow.com/questions/64402915/esp32-cam-with-servo-control-wont-work-arduino-ide
+  //and
+  //https://www.esp32.com/viewtopic.php?t=11379
+  servo_pan.attach(servoPin_pan, 4);
+  servo_tilt.attach(servoPin_tilt, 5);
 
-    servo_pan.write(90);
-    servo_tilt.write(90);
-    
-    Serial.begin(115200);
+  servo_pan.write(90);
+  servo_tilt.write(90);
 
-    camera_init();
+  Serial.begin(115200);
+
+  camera_init();
 
 
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-   
-    Serial.println("WiFi connected..!");
-    delay(100);
-    
-    Serial.print("Camera Stream Ready! Go to: http://");
-    Serial.print(WiFi.localIP());
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-    // Start streaming web server
-    startCameraServer();
-    ota_setup();
+  Serial.println("WiFi connected..!");
+  delay(100);
+
+  Serial.print("Camera Stream Ready! Go to: http://");
+  Serial.print(WiFi.localIP());
+
+  // Start streaming web server
+  startCameraServer(&processFrame);
+  ota_setup();
 }
 
-void loop(){
-    ArduinoOTA.handle();
+
+void loop() {
+  ota_loop();
+  // camera does not need to be in the loop - processFrame is called each time there is a new image.
 }
